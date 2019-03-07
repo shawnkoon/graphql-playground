9c05259c904fc17d2df0da28345f9f6e21c78d99
Shawn Choi <shawn.choi@us.imshealth.com>
2019-03-06 22:35:18 -0800

    Finished implementing basic Query related operations

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,73 @@
 import { GraphQLServer } from 'graphql-yoga';
 
-// Scalar String, Boolean, Int, Float, ID
+// Scalar Types - String, Boolean, Int, Float, ID
+
+// Demo user data
+const users = [
+  {
+    id: '1',
+    name: 'Shawnkoon',
+    email: 'shawnkoon@example.com',
+    age: 42
+  },
+  {
+    id: '2',
+    name: 'Curran',
+    email: 'curran@example.com',
+    age: 24
+  },
+  {
+    id: '3',
+    name: 'Kelly',
+    email: 'kelly@example.com'
+  }
+];
+
+const posts = [
+  {
+    id: '1',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL...',
+    published: false,
+    author: '1'
+  },
+  {
+    id: '2',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post...',
+    published: true,
+    author: '1'
+  },
+  {
+    id: '3',
+    title: 'TypeScript 101',
+    body: 'Basic TypeScript class to learn...',
+    published: false,
+    author: '2'
+  }
+];
+
+const comments = [
+  { id: '101', text: 'Wow, this is an awesome post!', author: '2', post: '1' },
+  { id: '102', text: 'Can I get more information about this?', author: '1', post: '3' },
+  { id: '103', text: 'I like the idea', author: '3', post: '2' },
+  { id: '104', text: 'Hire me', author: '1', post: '1' }
+];
 
 // Type definitions (schema)
 const typeDefs = `
   type Query {
-    greeting(name: String): String!
+    users(query: String): [User!]!
+    posts(query: String): [Post!]!
     me: User!
+    post: Post!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
     post: Post!
   }
 
@@ -15,6 +76,8 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
+    comments: [Comment!]!
   }
 
   type Post {
@@ -22,37 +85,64 @@ const typeDefs = `
     title: String!
     body: String!
     published: Boolean!
+    author: User!
+    comments: [Comment!]!
   }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    greeting(parent, args, context, info) {
-      console.log('parent', parent);
-      console.log('args', args);
-      console.log('context', context);
-      console.log('info', info);
-      if (args.name) {
-        return `Hello ${args.name}!`;
+    comments: (parent, args, context, info) => {
+      return comments;
+    },
+    users: (parent, args, context, info) => {
+      const { query } = args;
+      if (!query) {
+        return users;
       }
-
-      return 'hello';
+      return users.filter(user => user.name.toLowerCase().includes(query.toLowerCase()));
     },
-    me() {
-      return {
-        id: '12345',
-        name: 'Shawnkoon',
-        email: 'shawnkoon@example.com'
-      };
+    posts: (parent, args, context, info) => {
+      const { query } = args;
+      if (!query) {
+        return posts;
+      }
+      return posts.filter(
+        post =>
+          post.title.toLowerCase().includes(query.toLowerCase()) ||
+          post.body.toLowerCase().includes(query.toLowerCase())
+      );
     },
-    post() {
-      return {
-        id: '23456',
-        title: 'This is the title of the Post',
-        body: '',
-        published: false
-      };
+    me: () => {
+      return users[0];
+    },
+    post: () => {
+      return posts[0];
+    }
+  },
+  Comment: {
+    author: (parent, args, context, info) => {
+      return users.find(user => user.id === parent.author);
+    },
+    post: (parent, args, context, info) => {
+      return posts.find(post => post.id === parent.post);
+    }
+  },
+  Post: {
+    author: (parent, args, context, info) => {
+      return users.find(user => user.id === parent.author);
+    },
+    comments: (parent, args, context, info) => {
+      return comments.filter(comment => comment.post === parent.id);
+    }
+  },
+  User: {
+    posts: (parent, args, context, info) => {
+      return posts.filter(post => post.author === parent.id);
+    },
+    comments: (parent, args, context, info) => {
+      return comments.filter(comment => comment.author === parent.id);
     }
   }
 };
